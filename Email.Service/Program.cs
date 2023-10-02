02940c7f38ae72dfca688862f620a4e36b3fc6f2
ramalt <ramazanalltuntepe@gmail.com>
2023-10-02 18:30:54 +0300

    refactor

--- Email.Service/Program.cs
+++ Email.Service/Program.cs
@@ -1,3 +1,38 @@
-﻿
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
-Console.WriteLine("Hello, World!");
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "User Register Service";
+
+IConnection connection = factory.CreateConnection();
+IModel channel = connection.CreateModel();
+
+string exchangeName = "user.register";
+string queueName = "notification";
+string routingKey = "new-user";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, false, false, false, null);
+channel.QueueBind(queueName, exchangeName, routingKey);
+channel.QueueBind(queueName, exchangeName, routingKey);
+
+var consumer = new EventingBasicConsumer(channel);
+
+Console.WriteLine("email service running");
+
+consumer.Received += (sender, args) =>
+{
+    string message = Encoding.UTF8.GetString(args.Body.ToArray());
+    Console.WriteLine(message);
+    
+    channel.BasicAck(args.DeliveryTag, false);
+};
+
+string consumerTag = channel.BasicConsume(queueName,false, consumer);
+Console.ReadKey();
+
+channel.Close();
+connection.Close();
--- UserRegister.Service/Program.cs
+++ UserRegister.Service/Program.cs
@@ -1,2 +1,27 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using RabbitMQ.Client;
+
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "User Register Service";
+
+IConnection connection = factory.CreateConnection();
+IModel channel = connection.CreateModel();
+
+string exchangeName = "user.register";
+string queueName = "notification";
+string routingKey = "new-user";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, false, false, false, null);
+channel.QueueBind(queueName, exchangeName, routingKey);
+channel.QueueBind(queueName, exchangeName, routingKey);
+
+var messageBody = Encoding.UTF8.GetBytes("new user registered");
+channel.BasicPublish(exchangeName, routingKey, null, messageBody);
+
+Console.WriteLine("user register service running");
+Console.ReadKey();
+channel.Close();
+connection.Close();
